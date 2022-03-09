e0d7645a46cc8b26979224c2435dfdee40ffb520
PierreD <39910767+PierreDemailly@users.noreply.github.com>
2022-03-09 03:38:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 /**
  * Loop over generator and get for each item current index.
- * @generator
- * @yields [index, item].
- * @param {Generator} generator
- * 
  * @example
  * ```
     function *foo() {
@@ -18,7 +14,7 @@
     ```
  */
 // async await can be useless but may be usefull if generator passed is async.
-export async function *enunerate(generator) {
+async function *enunerate(generator) {
   let i = 0;
   for await (const item of generator) {
     yield [i++, item];
